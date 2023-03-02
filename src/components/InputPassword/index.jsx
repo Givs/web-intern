70a225bbf5b0eb1eef19f0c765ae5ae142f0c2d2
Givaldo Neto <valdingivs@outlook.com>
2023-03-02 11:42:33 -0300

    fix: pass correct value from password form

--- src/components/InputPassword/index.jsx
+++ src/components/InputPassword/index.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import classNames from 'clsx';
 
-export function InputPassword(){
+export function InputPassword(props){
     const [showPassword, setShowPassword] = useState(false);
+    const [password, setPassword] = useState('');
 
     const handleShowPassword = (e) => {
         e.preventDefault();
-        setShowPassword(!showPassword)
+        setShowPassword(!showPassword);
+    }
+
+    const newValue = e => {
+        props.onValueChange(password);
     }
 
     return (
@@ -16,10 +21,13 @@ export function InputPassword(){
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
                 autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onBlur={newValue}
             />
             <button
                 className={classNames(
-                    'absolute top-0 right-0 inset-y-0 right-0 px-2 py-1 text-gray-500 z-10',
+                    'absolute top-0 right-0 inset-y-0 px-2 py-1 text-gray-500 z-10',
                     {'focus:outline-none' : !showPassword}
                 )}
                 onClick={handleShowPassword}
